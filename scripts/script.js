c26f851f4b58039d9e39d21e9a2356da914448f1
LoqLamp <david04492@googlemail.com>
2022-07-05 12:27:56 +0100

    added function to generate grid

--- scripts/script.js
+++ scripts/script.js
@@ -5,15 +5,24 @@ sliderValue.addEventListener("input", () => {
 });
 
 let confirmButton = document.getElementById("confirm-button");
-confirmButton.addEventListener("click", buildCanvas);
+confirmButton.addEventListener("click", removeGrid);
+
+function removeGrid() {
+  while (canvas.hasChildNodes()) {
+    canvas.removeChild(canvas.firstChild);
+  }
+  buildCanvas();
+}
 
 function buildCanvas() {
-  let canvas = document.querySelector(".canvas");
+  let canvas = document.getElementById("canvas");
+  canvas.style.setProperty("--grid-size", sliderValue.value);
   let buildCanvasSize = sliderValue.value;
   let canvasSize = buildCanvasSize * buildCanvasSize;
+
   for (let i = 0; i < canvasSize; i++) {
-    cDiv = document.createElement("div");
-    cDiv.classList.add("canvas-div");
-    canvas.appendChild(cDiv);
+    canvasDiv = document.createElement("div");
+    canvasDiv.classList.add("canvas-div");
+    canvas.appendChild(canvasDiv);
   }
 }
