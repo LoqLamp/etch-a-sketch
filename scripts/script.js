c31ebbec7b1b2f7d0a398ca21a8efc3c7abd7013
LoqLamp <david04492@googlemail.com>
2022-07-05 16:15:38 +0100

    added function to change color on mouse over

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,10 @@
 let sliderValue = document.getElementById("slider-value");
+let greenButton = document.getElementById("green-button");
+
+greenButton.onclick = () => {
+  return greenButton === true ? (greenButton = false) : (greenButton = true);
+};
+
 sliderValue.addEventListener("input", () => {
   let sliderOutput = document.getElementById("slider-output");
   sliderOutput.textContent = sliderValue.value + " X " + sliderValue.value;
@@ -19,10 +25,27 @@ function buildCanvas() {
   canvas.style.setProperty("--grid-size", sliderValue.value);
   let buildCanvasSize = sliderValue.value;
   let canvasSize = buildCanvasSize * buildCanvasSize;
-
   for (let i = 0; i < canvasSize; i++) {
     canvasDiv = document.createElement("div");
     canvasDiv.classList.add("canvas-div");
     canvas.appendChild(canvasDiv);
+  }
+  addMouseListeners();
+}
+
+function addMouseListeners() {
+  let canvasDivEvent = document.querySelectorAll(".canvas-div");
+  canvasDivEvent.forEach((elements) => {
+    elements.addEventListener("mouseover", changeDivClass);
+  });
+}
+
+function changeDivClass(something) {
+  if (greenButton === true) {
+    something.target.className = "canvas-div";
+    something.target.classList.add("canvas-div-green");
+  } else {
+    something.target.className = "canvas-div";
+    something.target.classList.add("canvas-div-after");
   }
 }
